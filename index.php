fd30e06960db77a86892730926cb9663ecdd7cd7
Skyler Rexroad <skyrex1095@gmail.com>
2015-07-13 16:56:38 -0600

    Rewrote history

--- index.php
+++ index.php
@@ -5,14 +5,10 @@
 		</head>
 	<body>
 		<img src="images/aboutmeimg.jpg">
-		<p>I was born on June 28, 1985. I was born and raised in Dallas, TX. After a unsuccessful semester and a half of
-			community college I took a job at small municiple airport in Dallas. I then moved on to a series of jobs
-			including selling CutCo knives, running a stainless steel warehouse. For the past 3 years I've been managing a
-			ice hockey rink as well as coaching a team and teaching classes and private lesson for kids ages 4 to 18.</p>
-		<p>Along with playing hockey, I  enjoy rock climbing, reading and cooking. I recently quite my job to attend this
-			course. I took this risk because I reached a ceiling at the hockey rink and coaching is seasonal and sporatic.
-			I have always had an interest in coding and I see this course as a path to a rewarding career in a high demand
-			field. Which path my career will take is unkown. to be honest i dont know enough about coding yet to know what
-			I want to specialize in. Be it front end or back end or some combo of both.</p>
+		<p>Born at a young age, I knew from the start that I was special. That I had a destiny. I was to become...A CODEMONKEY.</p>
+		<p>My entire life has led to this moment. Eons of training have prepared me for the largest challenges. In order to prove my worth, I must pass the Deep Dive Coding Bootcamp!</p>
+		<p>
+			<small><small>/cheesy jokes</small></small>
+		</p>
 	</body>
 </html>

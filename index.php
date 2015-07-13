3203a8d05b9dd2cdae26ccf41b8f8dd1c0981fd7
James Huber <jhuber8@cmn.edu>
2015-07-13 16:36:14 -0500

    first test of about me project

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>All About James Huber</title>
+		</head>
+	<body>
+		<img src="aboutmeimg.jpg">
+		<p>I was born on June 28, 1985. I was born and raised in Dallas, TX. </p>
+	</body>
+</html>

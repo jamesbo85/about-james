cacadffbf1b8a2a51d928118cecf65c52f002529
James Huber <jhuber8@cmn.edu>
2015-07-13 16:58:28 -0500

    2nd test of about me project

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<img src="aboutmeimg.jpg">
 		<p>I was born on June 28, 1985. I was born and raised in Dallas, TX. After a unsuccessful semester and a half of
 			community college I took a job at small municiple airport in Dallas. I then moved on to a series of jobs
-			including selling Cutco knives, running a stainless steel warehouse. For the past 3 years I've been managing a
+			including selling CutCo knives, running a stainless steel warehouse. For the past 3 years I've been managing a
 			ice hockey rink as well as coaching a team and teaching classes and private lesson for kids ages 4 to 18.</p>
 		<p>Along with playing hockey, I enjoy rock climbing, reading and cooking. I recently quite my job to attend this
 			course. I took this risk because I reached a ceiling at the hockey rink and coaching is seasonal and sporatic.
